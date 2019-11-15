3777486e6f860f98e29ff262cb874245545ee6ee
dou1984 <dou1984@163.com>
2019-11-15 14:23:57 +0800

    mempool

---
+++ base/Klist.h
@@ -0,0 +1,94 @@
+#ifndef __KLIST_H__
+#define __KLIST_H__
+#include <stddef.h>
+
+namespace ashan
+{
+	template<class _Type>
+	class Klist
+	{
+		struct node_
+		{
+			node_* m_prev = NULL;
+			node_* m_next = NULL;		
+			operator node_*() { return this; }
+		};
+		template<class _T>
+		struct node : public node_
+		{
+			_T m_data;
+			template<class... _Args>
+			node(_Args&&... _args) : m_data(std::forward<_Args>(_args)...){}
+			void* operator new(size_t s)
+			{
+				auto p = malloc(s);
+				if (p != NULL)
+					return p;
+				throw("malloc error");
+			}
+			void operator delete(void* ptr) { free(ptr); }
+		};
+		node<int> m_node;
+		typedef node<_Type> node_t;
+	public:
+		Klist()
+		{
+			m_node.m_next = m_node.m_prev = m_node;
+			m_node.m_data = 0;
+		}
+		void push_front(node_t *new_) { __list_add(new_, m_node, m_node.m_next); }
+		void push_back(node_t *new_) { __list_add(new_, m_node.m_prev, m_node); }
+		void erase(node_t *old) { __list_del(old->m_prev, old->m_next); }
+		void replace(node_t *old, node_t *new_) { __replace(old, new_); }
+		bool empty()const { return m_node.m_data == 0; }
+		int size()const { return m_node.m_data; }
+		node_* begin() { return m_node.m_next; }
+		node_* end() { return m_node; }
+		node_t* front() { return (node_t*)m_node.m_next; }
+		node_t* back() { return (node_t*)m_node.m_prev; }
+		bool is_member(node_t* old)const
+		{
+			for (auto n = begin(); n != end(); n = n->m_next)
+				if (n == old)
+					return true;
+			return false;
+		}
+		template<class... _Args>
+		void emplace_back(_Args&&... _args)
+		{
+			push_back(new node_t(std::forward<_Args>(_args)...));
+		}
+		template<class... _Args>
+		void emplace_front(_Args&&... _args)
+		{
+			push_front(new node_t(std::forward<_Args>(_args)...));
+		}
+		template<class... _Args>
+		node_t* emplace(_Args&&... _args)
+		{
+			return new node_t(std::forward<_Args>(_args)...);
+		}
+	private:
+		void __list_add(node_ *cur, node_ *prev, node_ *next)
+		{
+			prev->m_next = next->m_prev = cur;
+			cur->m_next = next;
+			cur->m_prev = prev;
+			m_node.m_data++;
+		}
+		void __list_del(node_ * prev, node_ * next)
+		{
+			next->m_prev = prev;
+			prev->m_next = next;
+			m_node.m_data--;
+		}
+		void __replace(node_ *old, node_ *new_)
+		{
+			new_->m_next = old->m_next;
+			new_->m_prev = old->m_prev;
+			new_->m_next->m_prev = new_->m_prev->m_next = new_;
+		}
+	};
+}
+#endif
+
---
+++ base/Mempool.h
@@ -0,0 +1,88 @@
+#ifndef __MEMPOOL_H__
+#define __MEMPOOL_H__
+#include <array>
+#include "Klist.h"
+
+namespace ashan
+{
+	namespace __inner
+	{		
+		template<class _Type>
+		void* create(_Type& _obj);	
+		template<class _Type>
+		void destory(_Type& _obj, char* _ptr);		
+		template<class _Type>
+		void clear(_Type& _obj);		
+	}
+	template<size_t... _Size>
+	class Mempool;
+
+	template<size_t _Size>
+	class Mempool<_Size>
+	{
+		Klist<std::array<char, _Size>> m_data;
+	public:
+		virtual ~Mempool<_Size>() { __inner::clear(m_data); }
+		void* create(size_t s)
+		{
+			if (s <= _Size)
+				return __inner::create(m_data);
+			return malloc(s);
+		}
+		void destory(size_t s, void* ptr)
+		{
+			if (s <= _Size)
+				return __inner::destory(m_data, (char*)ptr);			
+			free(ptr);
+		}
+	};
+	template<size_t _Size, size_t... _Res>
+	class Mempool<_Size, _Res...> : public Mempool<_Res...>
+	{
+		Klist<std::array<char, _Size>> m_data;
+	public:
+		~Mempool<_Size, _Res...>() { __inner::clear(m_data); }
+		void* create(size_t s)
+		{
+			if (s <= _Size)
+				return __inner::create(m_data);
+			return Mempool<_Res...>::create(s);
+		}
+		void destory(size_t s, void* ptr)
+		{
+			if (s <= _Size)
+				return __inner::destory(m_data, (char*)ptr);
+			return Mempool<_Res...>::destory(s, ptr);
+		}
+	};	
+	namespace __inner
+	{
+		template<class _Type>
+		void* create(_Type& _obj)
+		{
+			if (_obj.empty())
+				return (_obj.emplace())->m_data.data();
+			auto p = _obj.front();
+			_obj.erase(p);
+			return p->m_data.data();
+		}
+		template<class _Type>
+		void destory(_Type& _obj, char* _ptr)
+		{
+			const auto _shift = ((size_t)&(((decltype(_obj.front()))0)->m_data));
+			_obj.push_back(decltype(_obj.front())(_ptr - _shift));
+		}
+		template<class _Type>
+		void clear(_Type& _obj)
+		{
+			while (!_obj.empty())
+			{
+				auto p = _obj.front();
+				_obj.erase(p);
+				delete p;
+			}
+		}
+	}
+}
+#endif
+
