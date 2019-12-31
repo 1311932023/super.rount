365405a53b98d0af3a563f7a6fdbdd9173ad3e91
Zhao YunShan <dou1984@163.com>
2019-12-31 12:01:25 +0800

    Update VTask.h
    提炼VTaskEx

--- base/VTask.h
+++ base/VTask.h
@@ -1,5 +1,8 @@
 #ifndef __VTASK_H__
 #define __VTASK_H__
+#include <list>
+#include <mutex>
+#include <atomic_int>
 
 class VTask 
 {
@@ -12,4 +15,51 @@ public:
 	virtual void operator()(void*) = 0;
 };
 
+class VTaskEx
+{
+protected:
+	enum E_TASK_STATUE	{ e_running, e_terminating, e_terminated,};		
+	std::list<VTask*> m_list;
+	std::mutex m_mutex;
+	std::atomic_int m_statue = e_running;
+public:
+	virtual ~VTaskEx() 
+	{
+		stop();
+		while (m_statue != e_terminated)
+			usleep(1000);
+	}
+	virtual void notify_task() = 0;
+	virtual void wait_task() = 0;	
+public:
+	void push_task(VTask* _task) 
+	{
+		{
+			std::lock_guard<decltype(m_mutex)> _lock(m_mutex);
+			m_list.push_back(_task);
+		}
+		notify_task();
+	}
+	void run_task()
+	{		
+		do
+		{
+			decltype(m_list) _list;
+			{
+				std::lock_guard<decltype(m_mutex)> _lock(m_mutex);
+				if (m_list.empty())
+					wait_task();
+				_list.swap(m_list);
+			}
+			for (auto it : _list)			
+				(*it)();			
+		}while(m_state == e_running);
+		m_state = e_terminated;
+	}
+	void stop_task()
+	{
+		m_statue = e_terminating;
+		notify_task();
+	}
+};
 #endif
